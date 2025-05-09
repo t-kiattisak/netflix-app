581ad16d0d84bd4b5822792f24b69910db3830b1
kiattisak jomram <t.kiattisak.j@gmail.com>
2025-05-09 11:34:26 +0700

    refactor: remove x-robots-tag header configuration from next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,17 +5,6 @@ const withNextIntl = createNextIntlPlugin()
 
 const nextConfig: NextConfig = {
   /* config options here */
-  headers: async () => [
-    {
-      source: "/(.*)",
-      headers: [
-        {
-          key: "x-robots-tag",
-          value: "noindex",
-        },
-      ],
-    },
-  ],
   images: {
     remotePatterns: [
       {
