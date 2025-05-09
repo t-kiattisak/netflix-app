4f5e2fa22a19f8011f716658e64f8bff2331d880
kiattisak jomram <t.kiattisak.j@gmail.com>
2025-05-09 16:21:15 +0700

    ✨ feat: add AppController with health check endpoint to improve API monitoring

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MovieModule } from './interfaces/http/movie/movie.module';
 import { GenreModule } from './interfaces/http/genre/genre.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [ConfigModule.forRoot(), MovieModule, GenreModule],
-  controllers: [],
+  controllers: [AppController],
   providers: [],
 })
 export class AppModule {}
